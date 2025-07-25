203fcfe4350e34d9ae5eb8135edbd8cd8e7a125c
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-07-25 20:35:33 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Automatic Update Check";
+        return "Automatic Restart Check";
     }
     
 }
