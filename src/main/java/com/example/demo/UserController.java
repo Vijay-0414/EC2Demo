ac923d968db5e92e8cf8f46d003ccbb483f8b4ad
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-07-25 10:10:43 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "After Update the JAR file with backup...First CI-CD Project using GitHub Actions";
+        return "First CI-CD Project using GitHub Actions";
     }
     
 }
