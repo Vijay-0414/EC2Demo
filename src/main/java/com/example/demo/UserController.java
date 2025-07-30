7b0cd366fcf39d6e599e3fdd112ce32761844702
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-07-30 17:00:03 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Deploying using Docker Image through Elastic Container Registry.... Developed by VJ";
+        return "Deploying using Docker Image through Elastic Container Registry.... Developed by VIJAYAPERUMAL";
     }
     
 }
