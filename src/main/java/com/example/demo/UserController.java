fc24e6320e420094aa7924a0c686915d3b6a6090
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-07-30 21:28:15 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Work Started successfully for to check Docker health...";
+        return "ECS Updating...";
     }
     
 }
