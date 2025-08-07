8cc1eb17731c3fee3ba05bfcc24ddb2c660b9637
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-07 13:38:07 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "ECS Updating...";
+        return "Finally Hosting in ECS with EC2...";
     }
     
 }
