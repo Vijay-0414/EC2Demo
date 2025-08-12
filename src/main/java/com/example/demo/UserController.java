3741d6cc88dedfc45c6ce41625ac7ae4a7f18b39
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-12 14:30:24 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Finally Updated Task Successfully...";
+        return "Version 2.0";
     }
     
 }
