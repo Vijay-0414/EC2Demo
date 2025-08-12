7042f4069740605a985d6a41d3954c7e9af90892
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-12 11:51:42 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "New Content Updation Checking....";
+        return "Again New Checking";
     }
     
 }
