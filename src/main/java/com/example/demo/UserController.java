fdc288838e59f53911009d62a2645a8c2a30e752
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-12 13:47:26 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "TASK DEFINITION ISSUE";
+        return "Updating........";
     }
     
 }
