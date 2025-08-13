3e450f7a2178c7b5aae9e06f8b8c47f0229cf221
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-13 20:12:17 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Docker Image Checking";
+        return "Task Updation";
     }
     
 }
