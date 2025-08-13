76320c3f759eb0a77e404c2d9501a2bb1740e1ae
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-13 07:31:51 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Finally Doing something now. We can Move for Next Process....";
+        return "Wednesday....";
     }
     
 }
