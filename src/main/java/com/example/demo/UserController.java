36d0cb3bd982ed6209985ac28d3ead8205f6d10d
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-13 19:39:00 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "OLD Container Removing Process.....";
+        return "NEW Container Removing Process.....";
     }
     
 }
