10bff6fd88bc2e12ba3ba77aab5dc88e4bc93f69
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-08-15 14:22:34 +0530

    Update UserController.java

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String demo(){
-        return "Friday Second Time Checking...";
+        return "Friday Third Time Checking...";
     }
     
 }
